753c88303d4ad164a79dfb0a07e8139f5dad0711
vagrant <mhyatt7@gatech.edu>
2020-10-03 00:22:38 +0000

    Haven't run test cases yet but finished

--- project_2_4/project_2_4.cpp
+++ project_2_4/project_2_4.cpp
@@ -11,16 +11,117 @@ struct LightOutputs {
 };
 
 struct LightInputs {
-  int time;
   bool car_sensor_n;
   bool car_sensor_s;
 };
 
 // TODO Define your light controller state machine class here
+class LightController {
+public:
+  LightOutputs update(LightInputs input) {
+    //take in input
+    switch (current_state) {
+      case (RR) :
+        if (input.car_sensor_s == 1 || input.car_sensor_n == 1) {
+          current_state = RG;
+          time_since_light_change += 1;
+        } else {
+          current_state = GR;
+          time_since_light_change += 1;
+        }
+        break;
+      case (GR) :
+        if (lightChange()) {
+          //change to yellow
+          current_state = YR;
+          time_since_light_change = 1;
+        } else {
+          time_since_light_change += 1;
+        }
+        break;
+      case (RG) :
+        if (lightChange()) {
+          //change to yellow
+          current_state = RY;
+          time_since_light_change = 1;
+        } else {
+          time_since_light_change = 1;
+        }
+        break;
+      case (YR) :
+        if (lightChange()) {
+          //change to red
+          current_state = RR;
+          time_since_light_change = 1;
+        } else {
+          time_since_light_change += 1;
+        }
+        break;
+      case (RY) :
+        if (lightChange()) {
+          current_state = RR;
+          time_since_light_change = 1;
+        } else {
+          time_since_light_change += 1;
+        }
+        break;
+    }
+    
+    return getLights();
+  }
+
+private:
+  int time_since_light_change = 1;
+
+  //declare states;
+  enum State {
+    RR,
+    GR,
+    RG,
+    RY,
+    YR
+  };
+
+  State current_state = RR;
+
+  //check if light can be changed
+  bool lightChange() {
+    if (current_state == GR || current_state == RG) {
+      return (time_since_light_change >= 5);
+    } else {
+      //at least one light is yellow
+      return (time_since_light_change >= 2);
+    }
+  }
+
+  //method to return LightOutputs from state
+  LightOutputs getLights() {
+    //checks all states (only 1 should be true)
+    switch(current_state) {
+      case (RR):
+        return {true, false, false, true, false, false};
+        break;
+      case (GR):
+        return {true, false, false, false, false, true};
+        break;
+      case (RG):
+        return {false, false, true, true, false, false};
+        break;
+      case (YR):
+        return {true, false, false, false, true, false};
+        break;
+      case (RY) :
+        return {false, true, false, true, false, false};
+        break;
+    }
+  }
+};
 
 int main()
 {
     // TODO Initialize your state machine class here
+    LightController controller;
+
     while(true)
     {
         std::string input;
@@ -29,10 +130,17 @@ int main()
             break;
 
         // TODO Parse the input digits
+        LightInputs inputs = {
+          input[0] == 1,
+          input[1] == 1
+        };
 
         // TODO Update your state machine based on input
+        LightOutputs output = controller.update(inputs);
 
         // TODO Output your state machine's light controls to std::cout
+        std::cout << output.red_ns << output.yellow_ns << output.green_ns
+                  << output.red_ew << output.yellow_ew << output.green_ew << "\n";
     }
     return 0;
 }
